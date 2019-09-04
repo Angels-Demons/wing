5e03b2b70171dc12e7bca98969c4c76b651184f8
Hassan Farahani <h.farahani@bahartel.ir>
2019-09-04 18:16:59 +0430

    Transactions admin and model fixed

--- zarinpal/admin.py
+++ zarinpal/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 
 from zarinpal.models import Transaction
 
-admin.site.register(Transaction)
+
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('user', 'authority', 'ref_id', 'amount', 'description', 'state', 'recharged', 'timestamp')
+
+
+admin.site.register(Transaction, TransactionAdmin)

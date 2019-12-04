2a161da4a313597f49f3855640e6e086fb9a5708
Hassan Farahani <h.farahani@bahartel.ir>
2019-12-04 09:33:07 +0330

    imei added

--- scooter/models.py
+++ scooter/models.py
@@ -143,7 +143,6 @@ class Scooter(models.Model):
     gps_board_connected = models.BooleanField(default=True, verbose_name="GPS Board")
     gps_valid = models.BooleanField(default=True)
     alerted = models.BooleanField(default=False)
-    imei = models.BigIntegerField(default=0)
 
     # def save(self, *args, **kwargs):
     #     ride = Ride.objects.filter(scooter=self, is_finished=False).first()
@@ -240,16 +239,16 @@ class Scooter(models.Model):
         # sms.lock_unlock(self.phone_number, False, self.device_code)
         mqtt.send_mqtt('scooter/' + str(self.phone_number), 'unlock')
         mqtt.send_mqtt('scooter/' + str(self.device_code), 'unlock')
-        if self.imei > 0:
-            unlock(str(self.imei))
+        if self.device_code > 999999:
+            unlock(str(self.device_code))
 
     # modify
     def turn_off(self):
         # sms.lock_unlock(self.phone_number, True, self.device_code)
         mqtt.send_mqtt('scooter/' + str(self.phone_number), 'lock')
         mqtt.send_mqtt('scooter/' + str(self.device_code), 'lock')
-        if self.imei > 0:
-            lock(str(self.imei))
+        if self.imei > 999999:
+            lock(str(self.device_code))
 
     def start_ride_atomic(self, user):
         try:
